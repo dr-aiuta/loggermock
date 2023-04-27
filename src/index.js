bd966a00b76e0804b108fbf536aff45dc44c979b
dr-aiuta <dr@aiuta.com.br>
2023-04-27 09:41:26 -0300

    dist folder

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-// app.ts
-const logger_1 = __importDefault(require("./logger/logger"));
-// Export logger as a default export.
-exports.default = logger_1.default;
